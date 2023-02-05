19b7f4d87f8accc9bef94b48e144431e094de769
Darren Pak <dapak@chapman.edu>
2023-02-05 00:25:54 -0800

    Update api.py

--- api.py
+++ api.py
@@ -47,7 +47,3 @@ def index():
     # A welcome message to test our server
     return "<h1>Welcome to our medium-greeting-api!</h1>"
 
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
